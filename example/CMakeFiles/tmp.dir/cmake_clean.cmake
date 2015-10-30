d6eeb7916b0649bac9ea5e4dd61d2476901009ab
TEJASWIKASARLA <tejaswi.kasarla1@gmail.com>
2015-10-30 21:45:55 +0530

    removed unnecessary folders

--- example/CMakeFiles/tmp.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/tmp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang)
-  include(CMakeFiles/tmp.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
